fea61b714d4ac3472f38244c5dcb78e7c7e1582f
theonote <theodecarvmvr@gmail.com>
2025-11-19 16:33:46 -0300

    Analise de Complexidade Final

--- AnalisedeComplexidade/florinda.c
+++ AnalisedeComplexidade/florinda.c
@@ -25,10 +25,18 @@ void weight(Candidatos* arr, int x){
     for(int i = 0; i < x-1; i++){
         for (int j = i+1; j < x; j++){
             if(arr[i].alt == arr[j].alt){
-                if (arr[i].pes > arr[j].pes && arr[i].pes != 75 && arr[j].pes != 75){
-                    Candidatos aux = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = aux;
+                if (arr[i].pes <= 75 && arr[j].pes <= 75){
+                    if ((arr[i].pes - 75) > (arr[j].pes - 75)){
+                        Candidatos aux = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = aux;
+                    }
+                } else {
+                    if ((arr[i].pes - 75) > (arr[j].pes - 75)){
+                        Candidatos aux = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = aux;
+                    }
                 }
             }
         }
@@ -72,24 +80,30 @@ void name(Candidatos* arr, int x){
 int main(){
     int n;
     Candidatos arr[100];
+
     scanf("%d", &n);
+
     for (int i = 0; i < n; i++){
         char nome[51];
         char sobrenome[51];
         int altura, peso;
+
         scanf("%s %s %d %d", nome, sobrenome, &altura, &peso);
+
         strcpy(arr[i].nome, nome);
         strcpy(arr[i].sobrenome, sobrenome);
         arr[i].alt = abs(180 - altura);
         arr[i].pes = peso;
     }
+
     height(arr, n);
     weight(arr, n);
     surname(arr, n);
     name(arr, n);
+
     for (int i = 0; i < n; i++){
-        printf("%s, %s", arr[i].sobrenome, arr[i].nome);
-        if (i != n-1) printf("\n");
+        printf("%s, %s\n", arr[i].sobrenome, arr[i].nome);
     }
+
     return 0;
 }
