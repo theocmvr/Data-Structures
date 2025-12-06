89b6306db78eaded57b1aa23edcf6fd597a0946b
theofps <theodecarvmvr@gmail.com>
2025-12-06 11:06:35 -0300

    Sorts

--- MinhasEstruturas/sorts/sorts.c
+++ MinhasEstruturas/sorts/sorts.c
@@ -10,7 +10,40 @@ void bubble(int* arr){
         }
     }
 }
-void merge(int* arr, int left, int mid, int right){
+void merge(int* arr, int l, int m, int r){
     int i, j, k;
-    int n1 = , n2;
+    int n1 = m-l+1, n2 = r-m;
+    int larr[n1], rarr[n2];
+    for (i = 0; i < n1; i++) larr[i] = arr[l+i];
+    for (j = 0; j < n2; j++) rarr[j] = arr[r+j+1];
+    i = j = 0;
+    k = l;
+    while (i < n1 && j < n2){
+        if (larr[i] <= rarr[j]){
+            arr[k] = larr[i];
+            i++;
+        } else {
+            arr[k] = rarr[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1) {
+        arr[k] = larr[i];
+        i++;
+        k++;
+    }
+    while (j < n2) {
+        arr[k] = rarr[j];
+        j++;
+        k++;
+    }
+}
+void mergesort(int* arr, int l, int r){
+    if (l < r){
+        int m = l+((r-l)/2);
+        mergesort(arr, l, m);
+        mergesort(arr, m+1, r);
+        merge(arr, l, m, r);
+    }
 }
--- MinhasEstruturas/sorts/sorts.h
+++ MinhasEstruturas/sorts/sorts.h
@@ -3,6 +3,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 void bubble(int* arr);
-void merge(int* arr);
-void mergesort(int* arr);
+void merge(int* arr, int l, int m, int r);
+void mergesort(int* arr, int l, int r);
 #endif
